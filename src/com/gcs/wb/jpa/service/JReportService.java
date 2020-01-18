1d022fda92356fadf661fd53b0cde11e5a998e05
Thang Phan <thangtp@hitachiconsulting.com>
2020-01-18 10:43:04 +0700

    Tafico update

---
+++ src/com/gcs/wb/jpa/service/JReportService.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gcs.wb.jpa.service;
+
+import com.gcs.wb.jpa.JReportConnector;
+import java.sql.Connection;
+import java.util.Map;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author thangtp.nr
+ */
+public class JReportService {
+    
+    Connection connect = JReportConnector.getInstance();
+    
+    Logger logger = org.apache.log4j.Logger.getLogger(this.getClass());
+
+ 
+
+    public void printReport(Map<String, Object> map, String reportName) {
+        try {
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reportName, map, connect);
+            JasperViewer jv = new JasperViewer(jasperPrint, false);
+            jv.setVisible(true);
+
+ 
+
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+    }
+}
