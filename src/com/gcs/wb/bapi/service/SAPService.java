2bd0e65c44c54dfd792ae48e0d4bc81e00a4b303
phov.as <phov@hitachiconsulting.com>
2020-01-31 18:22:37 +0700

    resolve conflict

--- src/com/gcs/wb/bapi/service/SAPService.java
+++ src/com/gcs/wb/bapi/service/SAPService.java
@@ -363,7 +363,72 @@ public class SAPService {
                 TransportAgentsConverter transportAgentsConverter = new TransportAgentsConverter();
                 transportSaps = transportAgentsConverter.convert(transports);
                 //sync SAP <=> DB
-                jpaService.syncTransportAgent(transportDBs, transportSaps);
+                /**
+                 * sync DVVC
+                 * @param transportDBs
+                 * @param transportSaps
+                 */
+                // delete data DB not exist SAP
+                for (TransportAgent transportAgent : transportDBs) {
+                    if (transportSaps.indexOf(transportAgent) == -1) {
+                        // delete in table Vehicle
+                        List<TransportAgentVehicle> transportAgentVehicles = transportAgentVehicleRepository.findByTransportAgentId(transportAgent.getId());
+
+                        if (!entityTransaction.isActive()) {
+                            entityTransaction.begin();
+                        }
+
+                        try {
+                            for (TransportAgentVehicle transportAgentVehicle : transportAgentVehicles) {
+                                entityManager.remove(transportAgentVehicle);
+                            }
+                        } catch (Exception ex) {
+                            JOptionPane.showMessageDialog(mainFrame, resourceMapMsg.getString("msg.deleteVehicleFalse"));
+                            entityTransaction.rollback();
+                            continue;
+                        }
+
+                        // delete dvvc
+                        try {
+                            if (!entityManager.contains(transportAgent)) {
+                                transportAgent = entityManager.merge(transportAgent);
+                            }
+                            entityManager.remove(transportAgent);
+                        } catch (Exception ex) {
+                            JOptionPane.showMessageDialog(mainFrame, resourceMapMsg.getString("msg.deleteProviderFalse"));
+                            entityTransaction.rollback();
+                            continue;
+                        }
+
+                        entityTransaction.commit();
+                        entityManager.clear();
+                    }
+                }
+
+                try {
+                    if (!entityTransaction.isActive()) {
+                        entityTransaction.begin();
+                    }
+                    // update dara SAP -> DB
+                    for (TransportAgent transportAgentSAP : transportSaps) {
+                        int index = transportDBs.indexOf(transportAgentSAP);
+                        if (index == -1) {
+                            entityManager.persist(transportAgentSAP);
+                        } else {
+                            transportAgentSAP.setId(transportDBs.get(index).getId());
+                            entityManager.merge(transportAgentSAP);
+                        }
+                    }
+
+                    entityTransaction.commit();
+                    entityManager.clear();
+
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(mainFrame, resourceMapMsg.getString("msg.syncProviderFalse"));
+                    if (entityTransaction.isActive()) {
+                        entityTransaction.rollback();
+                    }
+                }
             } catch (Exception ex) {
                 // to do
             }
